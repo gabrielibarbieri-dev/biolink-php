f46eb179ea0317b901eec3b9abc32525cfe71698
Barbieri <gabrieli.barbieri@aluno.senai.br>
2025-11-27 09:15:26 -0300

    1/2/3 ativades da etapa 2
    Criando loop com meus links pessoais

--- index.php
+++ index.php
@@ -1,27 +1,47 @@
 <?php
 
+$links = [
+"Instagram" => "https://instagram.com/gabbarbieri_",
+"WhatsApp" => "https://wa.me/17981157253",
+"Meu Jogo Favorito" => "https://play.google.com/store/apps/details?id=com.block.juggle&pcampaignid=web_share",
+"Música do Momento" => "https://youtu.be/kI6ywewtYkc?si=r3kiqAnNx2Kh0DPN"
+];
+// ESCOLHER O LINK VIP
+$vip = "Instagram";
+
 // --- DADOS DO PERFIL (PREENCHA COM SEUS DADOS) ---
 $nome = "Gabrieli";
-
-$bio = "i loved girassois<3";
+$bio = "i love girassois<3";
 $imagem = "https://plus.unsplash.com/premium_photo-1700124162812-1d5d29087b81?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"; // Pegue o link da SUA foto.
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.
-0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>BioLink de <?php echo $nome; ?></title>
 <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
 <div class="container">
-<img src="<?php echo $imagem; ?>" alt="Foto de Perfil" class="avat
-ar">
+<img src="<?php echo $imagem; ?>" alt="Foto de Perfil" class="avatar">
 <h1> <?php echo $nome; ?> </h1>
 <p> <?php echo $bio; ?> </p>
-<div class="lista-links"></div>
+<div class="lista-links">
+<?php
+foreach ($links as $texto => $url) {
+$classe_extra = "";
+if ($texto === $vip) {
+    $classe_extra = "destaque";
+}
+
+// DESAFIO: Se o texto for "WhatsApp" (ou outro que você queira), mude a variável $classe_extra para "destaque". Escreva o IF aqui:
+
+// O PHP constrói o botão HTML
+echo "<a href='$url' class='btn $classe_extra' target='_blank'>$texto</a>";
+}
+?>
 </div>
 </body>
 </html>
